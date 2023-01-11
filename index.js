a5ee069bba926bd2fc578e910f0cbb0f0126bf99
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-11 00:43:54 -0600

    Inquirer placement In this commit created a initial instruction sets (This is mainly for timing and placement Made main function in index.js async

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import HeroBanner from "./src/HeroBanner.js";
 import ClearScreen from "./src/ClearScreen.js";
+import inquirer from "inquirer";
+import Manager from "./lib/Manager.js";
 
-function main() {
+async function main() {
     //Main Title shows up
     //Clear the Screen First
     ClearScreen();
@@ -11,8 +13,27 @@ function main() {
 
     //Welcome Instructions, hit any key to proceed
     console.log('Welcome to the Manager\'s Elite Team Profile Generator');
-    //Enter the team managers name, employee ID, email address, and office number
+    console.log('This application will assist you in generating a Webpage with your team members information\nPlease answer all prompts in order to populate your team');
+    await inquirer.prompt([{name: 'pause', message: "Press Enter to continue"}]).then(() => ClearScreen());
     
+    //Enter the team managers name, employee ID, email address, and office number
+    let name, id, email, office
+    inquirer.prompt([
+        {
+            name: 'managerName',
+            message: 'What is the manager name?'
+        },
+        {
+            name: 'managerId',
+            message: 'Please enter an ID for the manager'
+        },
+        {
+            name: 'officeNumber',
+            message: 'What is the managers office number?'
+        }
+    ]).then( (answers) => {
+        console.log(`${answers.managerName}`);
+    })
     //Enter main loop  Enter Intern, Engineer or exit
 
     //Generate html
