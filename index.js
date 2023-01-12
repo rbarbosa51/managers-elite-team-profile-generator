2d866dc4c6bf9158b3a0cb6ef3b47f38acb3b14e
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-12 15:44:03 -0600

    Main Loop and Intern In this commit: I made several cosmetic changes to the appearance of the text. Created a main loop that asks if you finished inputting team members. I created the CreateIntern that creates an instance of Intern that gets returned and populated to the employees global variable

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ import ClearScreen from "./src/ClearScreen.js";
 import inquirer from "inquirer";
 import chalk from "chalk";
 import Manager from "./lib/Manager.js";
+import CreateIntern from './src/CreateIntern.js'
+import Intern from "./lib/Intern.js";
 
 //Global 
-let teamManager;
+//Employees vaiable will hold all of the created employees
+let employees = [];
+
 async function main() {
     //Main Title shows up
     //Clear the Screen First
@@ -15,8 +19,8 @@ async function main() {
     ClearScreen();
 
     //Welcome Instructions, hit any key to proceed
-    console.log('Welcome to the Manager\'s Elite Team Profile Generator');
-    console.log('This application will assist you in generating a Webpage with your team members information\nPlease answer all prompts in order to populate your team');
+    console.log(`Welcome to the ${chalk.bold.red('Manager\'s Elite Team Profile Generator')}`);
+    console.log(`This application will assist you in generating a ${chalk.bold.yellow('Webpage')} with your team members information\nPlease answer all prompts in order to populate your team`);
     await inquirer.prompt([{name: 'pause', message: "Press Enter to continue"}]).then(() => ClearScreen());
     
     //Enter the team managers name, employee ID, email address, and office number
@@ -40,11 +44,45 @@ async function main() {
         }
     ]).then( (answers) => {
         //name, id, email, officeNumber
-        teamManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber);
+        let teamManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber);
+        employees.push(teamManager);
     })
-    //console.log(teamManager);
+    //For debuggimg purposes
+    //console.log(employees);
     //Enter main loop  Enter Intern, Engineer or exit
-    
+    let finished = false;
+    let selection;
+    while (!finished) {
+        await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'mainLoopSelection',
+                message: 'Choose to add an Intern, Engineer, or Finsish Building your team:',
+                choices: ['Create an Intern', 'Create an Engineer', 'Finish Building your team']
+            }
+        ]). then(answer => {
+            switch (answer.mainLoopSelection) {
+                case 'Finish Building your team':
+                    console.log('You elected to finish building your team');
+                    finished = true;
+                    selection = 3;
+                    break;
+                case 'Create an Intern':
+                    selection = 1;
+                    break;
+                case 'Create an Engineer':
+                    selection = 2
+                    break;
+            }
+        });
+        switch (selection) {
+            case 1:
+                let intern = await CreateIntern();
+                employees.push(intern);
+        }
+    }
+    //Debugging
+    console.log(employees);
     //Generate html
 
 
---
+++ src/CreateIntern.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import Intern from "../lib/Intern.js";
+
+//This function prompts for information, then returns an instance of an Intern
+export default async function CreateIntern() {
+    let intern;
+    await inquirer.prompt([
+        {
+            name: 'internName',
+            message: `What is the Intern's ${chalk.bold.red('name')} ?}`
+        },
+        {
+            name: 'internId',
+            message: `What is the Intern's ${chalk.bold.red('ID')} ?}`
+        },
+        {
+            name: 'internEmail',
+            message: `What is the Intern's ${chalk.bold.red('Email')} address ?`
+        },
+        {
+            name: 'internSchool',
+            message: `What is the Intern's ${chalk.bold.red('School')} ?`
+        }
+    ]).then(answers => {
+        intern = new Intern(answers.internName,answers.internId, answers.internEmail, answers.internSchool );
+    })
+    return intern;
+}
