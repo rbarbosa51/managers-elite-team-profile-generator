7810ff07214312af29f8d8c15cd60d359715ae1b
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-14 16:04:51 -0600

    Intro to generating HTML Included icons from fontawesome for manager, engineer, and intern. Shaped the GenerateHTML.js file

--- src/GenerateHTML.js
+++ src/GenerateHTML.js
@@ -1,6 +1,9 @@
-import { Employee } from "../lib/Employee.js";
+//import { Employee } from "../lib/Employee.js";
 
-export default function GenerateHTML(employee) {
-    console.log('Inside GenerateHTML');
+export default function GenerateHTML(employees) {
+    //console.log('Inside GenerateHTML');
+    employees.forEach(emp => {
+        console.log(emp.getRole());
+    });
     return;
 }
